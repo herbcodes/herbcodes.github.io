5597218b7ffc4adc2f5b5842feda3f80a3b4f575
herbcodes <155074493+herbcodes@users.noreply.github.com>
2023-12-30 00:29:47 +0800

    update

--- index.php
+++ index.php
@@ -3,16 +3,19 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    
+    <!-- import -->
     <link href="bootstrap.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik+Doodle+Shadow&display=swap" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
     <script src="jquery-3.7.1.js"></script>
+    
+    <title>Bootstrap demo</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1 class="rubik_font">Hello, world!</h1>
     <script src="person.php"></script>
     <script src="bootstrap.bundle.js"></script>
     <script src="popper.js"></script>
