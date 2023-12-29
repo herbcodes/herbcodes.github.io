a7add947fb095368b9d5b0c03baf03a6074d2500
herbcodes <155074493+herbcodes@users.noreply.github.com>
2023-12-29 21:57:45 +0800

    update

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="bootstrap.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="bootstrap.css" rel="stylesheet">
     <script src="jquery-3.7.1.js"></script>
   </head>
   <body>
     <h1>Hello, world!</h1>
     <script src="person.php"></script>
-    <script src="bootstrap.bundle.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="popper.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="bootstrap.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="bootstrap.bundle.js"></script>
+    <script src="popper.js"></script>
+    <script src="bootstrap.js"></script>
   </body>
 </html>
