8b6ed2573fdec8bc4f400d656c58122127798388
herbcodes <155074493+herbcodes@users.noreply.github.com>
2024-01-02 22:35:28 +0800

    update file via upload
    update

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!-- import -->
     <link href="bootstrap.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -11,14 +12,50 @@
     <link href="https://fonts.googleapis.com/css2?family=Rubik+Doodle+Shadow&display=swap" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
     <script src="jquery-3.7.1.js"></script>
-    
+
     <title>Bootstrap demo</title>
-  </head>
-  <body>
-    <h1 class="rubik_font">Hello, world!</h1>
+</head>
+
+<body>
+    <!-- php -->
+    <?php
+    //
+    
+    //
+    ?>
+
+    <!-- main html body -->
+    <div class="container-lg">
+        <div class="text-center">
+            <h2>Registration Form</h2>
+        </div>
+        <div class="row justify-content-center my-5">
+            <div class="col-lg-6">
+                <form>
+                    <label for="email">Email Address:</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="myname@email.com">
+
+                    <label for="name">First Name: </label>
+                    <input type="text" class="form-control" id="firstname" id="firstname" placeholder="John">
+
+                    <label for="name">Last Name: </label>
+                    <input type="text" class="form-control" id="lastname" id="lastname" placeholder="Doe">
+
+                    <label for="name">Middle Initial: </label>
+                    <input type="text" class="form-control" id="middleinitial" id="middleinitial" placeholder="H.">
+
+                    <label for="name"></label>
+                    <input type="text" class="form">
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Import -->
     <script src="person.php"></script>
     <script src="bootstrap.bundle.js"></script>
     <script src="popper.js"></script>
     <script src="bootstrap.js"></script>
-  </body>
+</body>
+
 </html>
